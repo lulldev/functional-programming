14b5996264cadb8f11cc4e7dda8554aa0159fdd6
lulldev <lulldev@yandex.ru>
2018-09-24 12:38:15 +0300

    create lw4

---
+++ haskell/lw4/lw4.hs
@@ -0,0 +1,16 @@
+module LW4 (myAnd,myOr) where
+
+-- Set
+-- null
+-- fromList
+-- insert
+-- delete
+-- member
+
+myAnd :: [Bool] -> Bool
+{- здесь ваша реализация функций
+-}
+
+myOr :: [Bool] -> Bool
+{- здесь ваша реализация функций
+-}
